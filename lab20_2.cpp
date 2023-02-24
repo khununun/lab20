887ce789eeb1999e882ca2c4853d22a0a0df9a31
khununun <68842142+khununun@users.noreply.github.com>
2023-02-24 13:58:00 +0700

    Update lab20_2.cpp

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,20 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2)
+{
+	double Right = min((*R1).x + (*R1).w, (*R2).x + (*R2).w);
+	double Left = max((*R1).x, (*R2).x);
+	double Up = min((*R1).y, (*R2).y);
+	double Down = max((*R1).y - (*R1).h, (*R2).y - (*R2).h);
+	double area = (Right - Left) * (Up - Down);
+	if (area > 0)
+	{
+		return (Right - Left) * (Up - Down);
+	}
+	else
+	{
+		return 0;
+	}
 }
+
